542060626194ff0fb5af9490190c2c6e031533ec
gabygs <gabgongora@gmail.com>
2014-05-06 20:14:48 -0500

    object printing

--- src/main/java/itam/textingspark/spark_testing/testkm.scala
+++ src/main/java/itam/textingspark/spark_testing/testkm.scala
@@ -11,8 +11,8 @@ import java.io._
 
 class testkm {
 
-	//def main(args:Array[String]){
 	def getVW(k: Int, sourceFile:String){
+
 		//spark environment
 		val sc = new SparkContext("local","kmeans_spark","/usr/local/Cellar/spark-0.9.1/")
 		// master, appName, sparkHome, jars required
@@ -22,7 +22,6 @@ class testkm {
 		val parsedData = data.map(_.split(',').map(_.toDouble))
 
 		val maxIter = 50
-		//val k= 50
 
 		//save VW
 		val kmModel:KMeansModel = KMeans.train(parsedData, k, maxIter)
@@ -39,13 +38,44 @@ class testkm {
 		id=id+1
 		for( j <- 0 until c.length) {
 			printf("%.2f ", c(j))
-			bw.write(c(j).toString)
-			bw.write("\t")
+			bw.write(c(j).toInt.toString)
+			bw.write(" ")
 		}
 		bw.write("\n")
 		println();
 		}
 		bw.close()
+
+		val kmOut: FileOutputStream = new FileOutputStream("data/kmeans_model.obj")
+		val kmObjOut: ObjectOutputStream = new ObjectOutputStream(kmOut)
+		kmObjOut.writeObject(kmModel)
+		kmObjOut.close()
+
+		/*
+		val kmIn: FileInputStream = new FileInputStream("data/kmeans_model.obj")
+		val kmObjIn: ObjectInputStream = new ObjectInputStream(kmIn)
+		kmObjIn.readObject()
+		*/
+		
+		val kmObject = sc.objectFile("data/kmeans_model.obj")
+		
+		val data2 = sc.textFile("data/testeg.sift")
+		val parsedData2 = data2.map(_.split(',').map(_.toDouble))
+		
+
+		//val kmModel2:KMeansModel = kmObject.asInstanceOf[KMeansModel]
+
+		//val kmModel2:KMeansModel = kmObjIn => [KMeansModel]
+		
+		//val kmModel2 = kmObject.
+		
+//		val predData2 = parsedData2.map(x=>kmModel2.predict(x))
+//		predData2.saveAsTextFile("data/vw2")
+
+
+		  
+		//kmObjOut.close()
+		
 
 		sc.stop();
 	}
